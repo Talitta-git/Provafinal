3fe68911fcdecf95dd010d967897c8c490320407
talitta <talitabastoss@gmail.com>
2020-09-17 15:41:29 -0300

    prova

--- java.js
+++ java.js
@@ -4,11 +4,3 @@ function limparcampo(){
 	document.getElementById('ano').value=''
 	document.getElementById('materia').value=''
 }
-function salvar(){
-	document.getElementById('nome')
-	document.getElementById('data')
-	document.getElementById('ano')
-	document.getElementById('materia')
-
-
-}
