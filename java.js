b409c534cf9754aee69df16d369c30ea5db1e52b
talitta <talitabastoss@gmail.com>
2020-09-17 15:36:39 -0300

    prova

---
+++ java.js
@@ -0,0 +1,14 @@
+function limparcampo(){
+	document.getElementById('nome').value=''
+	document.getElementById('data').value=''
+	document.getElementById('ano').value=''
+	document.getElementById('materia').value=''
+}
+function salvar(){
+	document.getElementById('nome')
+	document.getElementById('data')
+	document.getElementById('ano')
+	document.getElementById('materia')
+
+
+}
